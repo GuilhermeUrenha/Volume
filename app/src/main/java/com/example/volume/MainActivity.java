20ed7df7c59d8fdcb50324aa3369ceeb22e8345d
GuilhermeUrenha <guilherme.urenha@gmail.com>
2020-05-22 21:45:38 -0300

    Revert "Revert "Volumes""
    This reverts commit a29e5dc3

---
+++ app/src/main/java/com/example/volume/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.volume;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtTitle;
+    TextView txtEsfera;
+    TextView txtPiramideB;
+    TextView txtPiramideA;
+
+    EditText edtEsferaRaio;
+    EditText edtPiramideBase;
+    EditText edtPiramideAlt;
+
+    double esferaR;
+    double piramideL;
+    double piramideA;
+
+    double VolumeEsfera;
+    double VolumePiramide;
+
+    String strEsfera;
+    String strPiramide;
+
+    public static final String MESSAGE1 = "com.example.volume.MESSAGE1";
+    public static final String MESSAGE2 = "com.example.volume.MESSAGE2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtEsfera = findViewById(R.id.txtEsfera);
+        txtPiramideB = findViewById(R.id.txtPiramideB);
+        txtPiramideA = findViewById(R.id.txtPiramideA);
+
+        txtTitle.setText(R.string.title);
+        txtEsfera.setText(R.string.esfera);
+        txtPiramideB.setText(R.string.piramideB);
+        txtPiramideA.setText(R.string.piramideA);
+
+        edtEsferaRaio = findViewById(R.id.edtEsfera);
+        edtPiramideBase = findViewById(R.id.edtPiramideB);
+        edtPiramideAlt = findViewById(R.id.edtPiramideA);
+    }
+
+    Intent intent;
+    public void postResultado(View view){
+        esferaR = Double.parseDouble(String.valueOf(edtEsferaRaio.getText()));
+        piramideL = Double.parseDouble(String.valueOf(edtPiramideBase.getText()));
+        piramideA = Double.parseDouble(String.valueOf(edtPiramideAlt.getText()));
+
+        VolumeEsfera = 1.00 / 3 * 3.1415 * Math.pow(esferaR, 3);
+        VolumePiramide = 1.00 / 3 * piramideL * piramideA;
+
+        strEsfera = String.valueOf(VolumeEsfera);
+        strPiramide = String.valueOf(VolumePiramide);
+
+        intent = new Intent(this, resultados.class);
+
+        intent.putExtra(MESSAGE1, strEsfera);
+        intent.putExtra(MESSAGE2, strPiramide);
+        startActivity(intent);
+    }
+}
